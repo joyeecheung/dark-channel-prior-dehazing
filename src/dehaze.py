932c94bce0b4cec9c262d56e2071b3591914fb10
Joyee Cheung <joyeec9h3@gmail.com>
2015-01-13 22:13:09 +0800

    guided filter

--- src/dehaze.py
+++ src/dehaze.py
@@ -3,6 +3,14 @@
 
 import numpy as np
 from PIL import Image
+from itertools import combinations_with_replacement
+from collections import defaultdict
+from numpy.linalg import inv
+
+from util import get_filenames
+
+R, G, B = 0, 1, 2
+
 
 def get_dark_channel(data, w):
     """Get dark channels from image data (RGB images only).
@@ -19,7 +27,7 @@ def get_dark_channel(data, w):
     """
     # from equation 5
     M, N, _ = data.shape
-    padded = np.pad(data, ((w/2, w/2), (w/2, w/2), (0, 0)), 'edge')
+    padded = np.pad(data, ((w / 2, w / 2), (w / 2, w / 2), (0, 0)), 'edge')
     darkch = np.zeros((M, N))
     for i, j in np.ndindex(darkch.shape):
         darkch[i, j] = np.min(padded[i:i + w - 1, j:j + w - 1, :])
@@ -41,23 +49,6 @@ def get_transmission(data, atmosphere, darkch, omega, w):
     return 1 - omega * get_dark_channel(newdata, w)
 
 
-def get_radiance(I, tmin=0.1, Amax=220, w=15, p=0.001, omega=0.95):
-    # equation 16
-    I = I.astype(np.float64)
-    m, n, _ = I.shape
-    Idark = get_dark_channel(I, w)
-    A = get_atmosphere(I, Idark, p)
-    t = get_transmission(I, A, Idark, omega, w)
-    t = np.repeat(t, 3).reshape(m, n, 3)
-    return (I - A)/t + A
-
-
-def dehaze_naive(im):
-    radiance = get_radiance(np.asarray(im))
-    radiance = np.maximum(np.minimum(radiance, 255), 0)
-    return Image.fromarray(radiance.astype(np.uint8))
-
-
 def boxfilter(data, r):
     """
     Parameters
@@ -69,13 +60,74 @@ def boxfilter(data, r):
     dest = np.zeros((M, N))
 
     sumY = np.cumsum(data, axis=0)
-    dest[:r+1] = sumY[r: 2*r+1]
-    dest[r+1:M-r] = sumY[2*r+1:] - sumY[:M-2*r-1]
-    dest[-r:] = np.tile(sumY[-1], (r, 1)) - sumY[M-2*r-1:M-r-1]
+    dest[:r + 1] = sumY[r: 2 * r + 1]
+    dest[r + 1:M - r] = sumY[2 * r + 1:] - sumY[:M - 2 * r - 1]
+    dest[-r:] = np.tile(sumY[-1], (r, 1)) - sumY[M - 2 * r - 1:M - r - 1]
 
     sumX = np.cumsum(dest, axis=1)
-    dest[:, :r+1] = sumX[:, r:2*r+1]
-    dest[:, r+1:N-r] = sumX[:, 2*r+1:] - sumX[:, :N-2*r-1]
-    dest[:, -r:] = np.tile(sumX[:, -1][:, None], (1, r)) - sumX[:, N-2*r-1:N-r-1]
+    dest[:, :r + 1] = sumX[:, r:2 * r + 1]
+    dest[:, r + 1:N - r] = sumX[:, 2 * r + 1:] - sumX[:, :N - 2 * r - 1]
+    dest[:, -r:] = np.tile(sumX[:, -1][:, None], (1, r)) - \
+        sumX[:, N - 2 * r - 1:N - r - 1]
     return dest
 
+
+def guided_filter(I, p, r=20, eps=1e-3):
+    M, N = p.shape
+    base = boxfilter(np.ones((M, N)), r)
+
+    means = [boxfilter(I[:, :, i], r) / base for i in xrange(3)]
+    mean_p = boxfilter(p, r) / base
+    means_IP = [boxfilter(I[:, :, i] * p, r) / base for i in xrange(3)]
+    covIP = [means_IP[i] - means[i] * mean_p for i in xrange(3)]
+
+    var = defaultdict(dict)
+    for i, j in combinations_with_replacement(range(3), 2):
+        var[i][j] = boxfilter(
+            I[:, :, i] * I[:, :, j], r) / base - means[i] * means[j]
+
+    a = np.zeros((M, N, 3))
+    for y, x in np.ndindex(M, N):
+        Sigma = np.array([[var[R][R][y, x], var[R][G][y, x], var[R][B][y, x]],
+                          [var[R][G][y, x], var[G][G][y, x], var[G][B][y, x]],
+                          [var[R][B][y, x], var[G][B][y, x], var[B][B][y, x]]])
+        cov = np.array([c[y, x] for c in covIP])
+        a[y, x] = np.dot(cov, inv(Sigma + eps * np.eye(3)))  # eq 14
+
+    b = mean_p - a[:, :, R] * means[R] - \
+        a[:, :, G] * means[G] - a[:, :, B] * means[B]
+
+    # eq 16
+    q = (boxfilter(a[:, :, R], r) * I[:, :, R] + boxfilter(a[:, :, G], r) *
+         I[:, :, G] + boxfilter(a[:, :, B], r) * I[:, :, B] + boxfilter(b, r)) / base
+
+    return q
+
+
+def get_radiance(I, tmin=0.1, w=41, p=0.001,
+                 omega=0.95, guided=False):
+    # equation 16
+    m, n, _ = I.shape
+    Idark = get_dark_channel(I, w)
+    A = get_atmosphere(I, Idark, p)
+    t = get_transmission(I, A, Idark, omega, w)
+    if guided:
+        t = guided_filter(I, t)
+    t = np.maximum(t, tmin)
+    t = np.repeat(t, 3).reshape(m, n, 3)
+    return (I - A) / t + A
+
+
+def dehaze(im, guided=True):
+    radiance = get_radiance(np.asarray(im, dtype=np.float64),
+                            guided=guided)
+    radiance = np.maximum(np.minimum(radiance, 255), 0)
+    return Image.fromarray(radiance.astype(np.uint8))
+
+
+if __name__ == '__main__':
+    for src, dest in get_filenames():
+        print 'processing', src
+        im = Image.open(src)
+        dehaze(im).save(dest)
+        print 'saved', dest
--- src/util.py
+++ src/util.py
@@ -6,6 +6,7 @@ import numpy as np
 from PIL import Image
 
 SRC_DIR = 'img'
+DEST_DIR = 'result'
 IMG_NAMES = ('canon7.jpg', 'cones.jpg', 'IMG_8763.jpg', 'IMG_8766.jpg',
              'ny12_photo.jpg', 'ny17_photo.jpg', 'pumpkins.jpg',
              'stadium1.jpg', 'toys.jpg', 'yellowmountain.jpg')
@@ -16,44 +17,7 @@ def get_filenames():
     file_dir = os.path.dirname(os.path.realpath(__file__))
     parent_dir, _ = os.path.split(file_dir)
     src_path = os.path.join(parent_dir, SRC_DIR)
-    filenames = map(lambda name: os.path.join(src_path, name),
-                    IMG_NAMES)
+    dest_path = os.path.join(parent_dir, DEST_DIR)
+    filenames = zip((os.path.join(src_path, name) for name in IMG_NAMES),
+                    (os.path.join(dest_path, name) for name in IMG_NAMES))
     return filenames
-
-
-def img_to_array(img, dtype=None):
-    """Convert an image to an 2-d array.
-
-    Parameters
-    ----------
-    img: the input image
-    dtype: the data type of the output array, if not specified,
-           it will use the smallest data type that can hold all the data
-           without overflow or underflow.
-
-    Return
-    ----------
-    If the mode of the image is RGB, then three 2-d arrays will be returned
-    """
-    if img.mode == 'RGB':
-        # convert each channel to a numpy array
-        return [img_to_array(ch) for ch in img.split()]
-    else:
-        return np.array(img.getdata(), dtype=dtype).reshape(img.size[::-1])
-
-
-def array_to_img(data, mode=None):
-    """Convert a 2-d numpy array to an image.
-
-    Parameters
-    ----------
-    img: the input image
-    mode:
-    """
-    if not mode:
-        return Image.fromarray(data)
-    elif mode == 'RGB':
-        channels = [array_to_img(ch, 'L') for ch in data]
-        return Image.merge('RGB', channels)
-    else:
-        return Image.fromarray(data).convert(mode)
